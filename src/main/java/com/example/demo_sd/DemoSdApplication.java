ca52eaec80276324d19e5e6979a7670d1a3de369
Desy1803 <101573725+Desy1803@users.noreply.github.com>
2024-09-22 18:44:03 +0200

    dependencies added

---
+++ src/main/java/com/example/demo_sd/DemoSdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSdApplication.class, args);
+	}
+
+}
